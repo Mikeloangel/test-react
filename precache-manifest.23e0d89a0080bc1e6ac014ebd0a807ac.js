b107ae31a1501d1ea170bd709ff55fc10a9dec77
Mikhail Varushichev <mikeloangel@gmail.com>
2022-11-17 13:45:39 +0100

    Updates

---
+++ precache-manifest.23e0d89a0080bc1e6ac014ebd0a807ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea36acaad9a3c60739f6f0db12cc995",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "e28bdfecc21ab2cffb73",
+    "url": "/test-react/static/css/main.55d8d7ea.chunk.css"
+  },
+  {
+    "revision": "9e906e243c39d79bf75b",
+    "url": "/test-react/static/js/2.f3039517.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test-react/static/js/2.f3039517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28bdfecc21ab2cffb73",
+    "url": "/test-react/static/js/main.3fbff684.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/test-react/static/js/main.3fbff684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae52eb296433dad8905",
+    "url": "/test-react/static/js/runtime-main.ec1d59aa.js"
+  },
+  {
+    "revision": "de9939530343274d6900535776b42e15",
+    "url": "/test-react/static/media/spinner.de993953.gif"
+  }
+]);
